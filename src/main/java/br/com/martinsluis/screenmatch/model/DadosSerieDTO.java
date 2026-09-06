25e8f033ea4cc06588f6b45a9e0cb24e2a2243b6
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-06 10:00:03 -0300

    feat(DadosSerieDTO): cria dto para serie

---
+++ src/main/java/br/com/martinsluis/screenmatch/model/DadosSerieDTO.java
@@ -0,0 +1,8 @@
+package br.com.martinsluis.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerieDTO(
+        @JsonAlias("Title")String titulo,
+        @JsonAlias("totalSesons")Integer totalEmporadas,
+        @JsonAlias("imdbRating")String avaliacao){}
