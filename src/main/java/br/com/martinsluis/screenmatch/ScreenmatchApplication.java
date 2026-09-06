e59bcd4c32e1aa0d2e5fa076181888aea7a41980
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-06 10:31:17 -0300

    feat(ScreenmatchApplication): cria instancia de ConverterDados e DadosSerieDTO para obter o json e converter da forma desejada

--- src/main/java/br/com/martinsluis/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/martinsluis/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.martinsluis.screenmatch;
 
+import br.com.martinsluis.screenmatch.model.dto.DadosSerieDTO;
 import br.com.martinsluis.screenmatch.service.ConsumoAPI;
+import br.com.martinsluis.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +17,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception{
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&seson=1&apikey=11491604");
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=11491604");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerieDTO dados = conversor.obterDados(json, DadosSerieDTO.class);
+		System.out.println(dados);
 	}
 
 }
