e90559b22c286878e802d64a6f351ba8760da4d6
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-08 11:35:21 -0300

    feat(DadosEpisodioDTO): cria dto para episodios

---
+++ src/main/java/br/com/martinsluis/screenmatch/model/dto/DadosEpisodioDTO.java
@@ -0,0 +1,12 @@
+package br.com.martinsluis.screenmatch.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodioDTO (
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Episode") Integer numero,
+        @JsonAlias("imdbRating")String avaliacao,
+        @JsonAlias("Released")String dataLancamento
+){}
