c263ac73017d99d273ddeaadfb965d71e03f3bf6
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-08 14:00:04 -0300

    refactor(ScreenmatchApplication): substitui código de obterDados por chamada do metodo exibeMenu da classe Principal

--- src/main/java/br/com/martinsluis/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/martinsluis/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.martinsluis.screenmatch;
 
-import br.com.martinsluis.screenmatch.model.dto.DadosEpisodioDTO;
-import br.com.martinsluis.screenmatch.model.dto.DadosSerieDTO;
-import br.com.martinsluis.screenmatch.model.dto.DadosTemporadaDTO;
-import br.com.martinsluis.screenmatch.service.ConsumoAPI;
-import br.com.martinsluis.screenmatch.service.ConverteDados;
+import br.com.martinsluis.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,26 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception{
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=11491604");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerieDTO dados = conversor.obterDados(json, DadosSerieDTO.class);
-		System.out.println(dados);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=11491604");
-		DadosEpisodioDTO dadosEpisodio = conversor.obterDados(json, DadosEpisodioDTO.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporadaDTO> temporadas = new ArrayList<>();
-
-		for(int i=1; i<=dados.totalTemporadas(); i++){
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season="+ i +"&apikey=11491604");
-			DadosTemporadaDTO dadosTemporada = conversor.obterDados(json, DadosTemporadaDTO.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+        Principal principal = new Principal();
+        principal.exibeMenu();
 	}
 
 }
