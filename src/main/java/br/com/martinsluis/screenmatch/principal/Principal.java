57797aa8098e9caf3b90a2dba25089a33f88ea45
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 18:36:51 -0300

    feat(Principal): cria lista de DadosEpisodioDTO que contem os 5 melhores episodios da serie

--- src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
+++ src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
@@ -7,8 +7,10 @@ import br.com.martinsluis.screenmatch.service.ConsumoAPI;
 import br.com.martinsluis.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner sc = new Scanner(System.in);
@@ -35,5 +37,17 @@ public class Principal {
         temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
+
+        List<DadosEpisodioDTO> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                //.toList(); //lista imutavel
+                .collect(Collectors.toList());
+
+        System.out.println("Top 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodioDTO::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
