984911b3e83855dea8439d970d2d3ffff6af23af
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-09 19:20:15 -0300

    feat(Principal): busca episodios a partir de uma data informada pelo usuário e esibe dados do episodio com data em formato dd/MM/yyyy

--- src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
+++ src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.martinsluis.screenmatch.model.dto.DadosTemporadaDTO;
 import br.com.martinsluis.screenmatch.service.ConsumoAPI;
 import br.com.martinsluis.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -54,8 +56,24 @@ public class Principal {
         List<Episodio> episodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream()
                         .map(d -> new Episodio(t.numero(), d)))
-                        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = sc.nextInt();
+        sc.nextLine(); //limpar buffer
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter fomatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(fomatador)
+                ));
     }
 }
