2cb25cb004045af7c91e181bd815eeeb53934275
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-12 13:58:51 -0300

    feat(Principal): usa o DoubleSummaryStatistics para mostrar algumas estatisticas basicas sobre a série

--- src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
+++ src/main/java/br/com/martinsluis/screenmatch/principal/Principal.java
@@ -94,5 +94,14 @@ public class Principal {
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
 
         System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e-> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episodio: " +est.getMax());
+        System.out.println("Pior episodio: " +est.getMin());
+        System.out.println("Quantidade: " +est.getCount());
     }
 }
